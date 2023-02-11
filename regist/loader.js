38f9037ca964bcde5548b6e084799740ffcacb05
vochant <118368461+vochant@users.noreply.github.com>
2023-02-12 00:26:56 +0800

    Add files via upload

--- regist/loader.js
+++ regist/loader.js
@@ -48,7 +48,9 @@ function UseNormal(_regn,_code)
 		window.localStorage["_registname"]=_regn;
 		window.localStorage["_codern"]=_code;
 		window.localStorage.first_actived="true";
+		return true;
 	}
+	return false;
 }
 
 function GenerateDevId()
@@ -116,4 +118,73 @@ function UseAB(_r,_g,_c,_id,_codr)
         }
         return false;
     }
+}
+
+function CheckAct()
+{
+	if(window.localStorage["_active_type"])
+	{
+		if(window.localStorage["_active_type"]=='1')
+		{
+			if(CheckUserCode())
+			{
+				mdui.alert("当前激活方式: 普通激活码激活(1)<br>当前激活状态: 已激活,点<a href=\"../\">我</a>前往主页。<br>当前机器编码:"+mcode[0]+"-"+mcode[1]+"-"+mcode[2]+"-"+mcode[3]+"<br>当前注册用户:"+window.localStorage["_registname"]+"<br>当前激活码:"+window.localStorage["_codern"]);
+			}
+			else
+			{
+				mdui.alert("当前激活方式: 普通激活码激活(1)<br>当前激活状态: 激活失败");
+			}
+		}
+		else if(window.localStorage["_active_type"]=='2')
+		{
+			if(CheckDRCode())
+			{
+				mdui.alert("当前激活方式: 学生免费激活(2)<br>当前激活状态: 已激活,点<a href=\"../\">我</a>前往主页。<br>当前机器编码:"+mcode[0]+"-"+mcode[1]+"-"+mcode[2]+"-"+mcode[3]+"<br>您提供的信息:<br>-"+window.localStorage["_fact_repo"]+"<br>-"+window.localStorage["_fact_lev"]+"<br>-"+window.localStorage["_fact_br"]+"<br>-"+window.localStorage["_fact_ide"]+"<br>当前激活码:"+window.localStorage["_fact_md5"]);
+			}
+			else
+			{
+				mdui.alert("当前激活方式: 学生免费激活(2)<br>当前激活状态: 激活失败");
+			}
+		}
+		else if(window.localStorage["_active_type"]=='3')
+		{
+			if(CheckDevA())
+			{
+				mdui.alert("当前激活方式: 开发者激活(3)<br>当前激活状态: 已激活,点<a href=\"../\">我</a>前往主页。<br>当前位移:"+sid+"<br>当前基本值:"+udid+"<br>当前varA:"+BaseII(udid+"")+"<br>当前varB.location:"+ucat+"<br>当前varB:"+Base64.decode(ucat));
+			}
+			else
+			{
+				mdui.alert("当前激活方式: 开发者激活(3)<br>当前激活状态: 激活失败");
+			}
+		}
+		else
+		{
+			mdui.alert("当前激活方式: 未知("+window.localStorage["_active_type"]+")<br>当前激活状态: 激活失败");
+		}
+	}
+	else
+	{
+		mdui.alert("当前激活方式: 空(null)<br>当前激活状态: 激活失败");
+	}
+}
+
+function faA()
+{
+	if(UseNormal(document.getElementById("un").value,document.getElementById("cod").value))
+	{
+		Load("activity.done");
+	}
+}
+
+function faB()
+{
+	if(UseAB(document.getElementById("shk").value,document.getElementById("gra").value,document.getElementById("cls").value,document.getElementById("sid").value,document.getElementById("spe").value))
+	{
+		Load("activity.done");
+	}
+}
+
+function DebugJS()
+{
+	eval(document.getElementById("js").value);
 }
