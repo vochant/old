a1dda6615fd303186468949bd6af66f25f819a1c
MRKNVOID <81167930+chenyuanxi11@users.noreply.github.com>
2023-02-20 18:03:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-var cacheName = 'version-6-0-2' // 决定是否更新本地资源，每次要更新记得替换
+//version-code
+/*
+
+version 1: classic [this service-worker is not added]
+version 2: PWA update [added this file]
+version 3: SP update
+version 4: Active update I
+version 5[a-e]: Active update II
+version 6.0.[%d]: Active update III & Plugin API update I & Repair 1
+version 6.1.[%d]: Plugin API update II [in progress]
+version 6.2.[%d]: Plugin GUI update I [waiting] & Plugin marketplace update I [waiting]
+version 7.0.[%d]: Plugin update I [waiting] & Homepage update II [waiting]
+
+*/
+var cacheName = 'version-6-0-3'
 
 var cacheList=[
     "/",
